321062d7ec6f66110bdea0be6b95d6d47201b9c1
skdev <skdev@ucsc.edu>
2025-07-14 08:35:35 -0700

    consistent database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
+import os
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-DATABASE_URL = "sqlite:///./codeeval.db"
+# Choose DB location based on environment
+if os.getenv("RENDER"):  # Render sets this env var automatically
+    db_path = "/data/codeeval.db"
+else:
+    db_path = "./codeeval.db"
+
+DATABASE_URL = f"sqlite:///{db_path}"
 
 database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
